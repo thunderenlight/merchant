1ea81faf0536379bb28c36d20fecf1acaf2f0646
thunderenlight <thunderenlight@gmail.com>
2020-03-13 01:37:59 -0500

    Implementing iteration 1with stock added to product model

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,17 @@
 module ProductsHelper
+
+	def print_price(price)
+		number_to_currency price
+	end
+
+	def print_stock(stock)
+		if stock > 0
+			content_tag(:span, "in stock (#{stock})", class: "in_stock")
+
+			#content_tag(:div, content_tag(:p, "Hello world!"), class: "strong")
+
+		else
+			content_tag(:span, "out of stock", class "out_of_stock")
+		end
+	end
 end
